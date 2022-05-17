7236ae4cb6df55f77755ba5cf91f29d4394e1c50
saragalanenriquez <102614980+saragalanenriquez@users.noreply.github.com>
2022-05-17 09:42:36 +0200

    Delete scr directory

--- scr/variables.py
+++
@@ -1,6 +0,0 @@
-import random
-import numpy as np
-tablero_jugador_barcos = np.full(fill_value='~', shape=(10, 10))
-tablero_jugador_disparos = np.full(fill_value='~', shape=(10, 10))
-tablero_maquina_barcos = np.full(fill_value='~', shape=(10, 10))
-tablero_maquina_disparos = np.full(fill_value='~', shape=(10, 10))
